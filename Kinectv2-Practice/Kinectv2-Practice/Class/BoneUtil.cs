d47f36704e1d35059aa8d6be7c1324270ca36188
赤木詠滋 <s1223077@cce.kanagawa-it.ac.jp>
2015-08-22 05:41:05 +0900

    色々追加してみたり

---
+++ Kinectv2-Practice/Kinectv2-Practice/Class/BoneUtil.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace Kinectv2_Practice.Class
+{
+    /// <summary>
+    /// 骨格情報を使用するのに必要な変数群、サンプルからコピペ、コメントを全部翻訳してみる
+    /// </summary>
+    class BoneUtil
+    {
+        /// <summary>
+        /// Radius of drawn hand circles
+        /// 描かれた手の円の半径（画面に表示される手の円の半径？）
+        /// </summary>
+        private const double HandSize = 30;
+
+        /// <summary>
+        /// Thickness of drawn joint lines
+        /// 描かれたジョイント線の厚さ(描画する円の厚さだと思われ)
+        /// </summary>
+        private const double JointThickness = 3;
+
+        /// <summary>
+        /// Thickness of clip edge rectangles
+        /// 切り取る四角形のエッジの厚さ(?) 
+        /// </summary>
+        private const double ClipBoundsThickness = 10;
+
+        /// <summary>
+        /// Constant for clamping Z values of camera space points from being negative
+        /// カメラ空間のZ軸が負数にならないための定数
+        /// </summary>
+        private const float InferredZPositionClamp = 0.1f;
+
+        /// <summary>
+        /// Brush used for drawing hands that are currently tracked as closed
+        /// </summary>
+        private readonly Brush handClosedBrush = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
+
+        /// <summary>
+        /// Brush used for drawing hands that are currently tracked as opened
+        /// </summary>
+        private readonly Brush handOpenBrush = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
+
+        /// <summary>
+        /// Brush used for drawing hands that are currently tracked as in lasso (pointer) position
+        /// </summary>
+        private readonly Brush handLassoBrush = new SolidColorBrush(Color.FromArgb(128, 0, 0, 255));
+
+        /// <summary>
+        /// Brush used for drawing joints that are currently tracked
+        /// </summary>
+        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
+
+        /// <summary>
+        /// Brush used for drawing joints that are currently inferred
+        /// </summary>        
+        private readonly Brush inferredJointBrush = Brushes.Yellow;
+
+        /// <summary>
+        /// Pen used for drawing bones that are currently inferred
+        /// </summary>        
+        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+
+        /// <summary>
+        /// Drawing group for body rendering output
+        /// </summary>
+        private DrawingGroup drawingGroup;
+
+        /// <summary>
+        /// Drawing image that we will display
+        /// </summary>
+        private DrawingImage imageSource;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Coordinate mapper to map one type of point to another
+        /// </summary>
+        private CoordinateMapper coordinateMapper = null;
+
+        /// <summary>
+        /// Reader for body frames
+        /// </summary>
+        private BodyFrameReader bodyFrameReader = null;
+
+        /// <summary>
+        /// Array for the bodies
+        /// </summary>
+        private Body[] bodies = null;
+
+        /// <summary>
+        /// definition of bones
+        /// </summary>
+        private List<Tuple<JointType, JointType>> bones;
+
+        /// <summary>
+        /// Width of display (depth space)
+        /// </summary>
+        private int displayWidth;
+
+        /// <summary>
+        /// Height of display (depth space)
+        /// </summary>
+        private int displayHeight;
+
+        /// <summary>
+        /// List of colors for each body tracked
+        /// </summary>
+        private List<Pen> bodyColors;
+
+        /// <summary>
+        /// Current status text to display
+        /// </summary>
+        private string statusText = null;
+    }
+}
---
+++ Kinectv2-Practice/Kinectv2-Practice/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+/*取りあえず色々なサイトを参考に名前空間の読み込み*/
+using System.ComponentModel;
+using Microsoft.Kinect;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+/*ここまで*/
+
+namespace Kinectv2_Practice
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /*キネクトに必要な変数群の宣言*/
+        KinectSensor kinect;
+        BodyFrameReader bodyFrameReader;//現状死に体
+        ColorFrameReader colorFrameReader = null;
+        ColorImageFormat colorImageFormat;
+        /// <summary>
+        /// フレームの説明情報を保有、つまりフレームの情報を格納するための場所じゃないかと
+        /// </summary>
+        FrameDescription colorFrameDescription;
+        Body[] bodies;
+        /*宣言終わり*/
+
+        int width, height ;
+        uint bytesPerPixel;
+
+        /// <summary>
+        /// 起動したときに一回だけ自動実行されるよ！（念のため）
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                ///キネクト本体の接続を確保、たしか接続されてない場合はfalseとかになった記憶
+                this.kinect = KinectSensor.GetDefault();
+                ///読み込む画像のフォーマット(rgbとか)を指定、どうやって読み込むかのリーダの設定も
+                this.colorImageFormat = ColorImageFormat.Bgra;
+                this.colorFrameDescription = this.kinect.ColorFrameSource.CreateFrameDescription(this.colorImageFormat);
+                this.colorFrameReader = this.kinect.ColorFrameSource.OpenReader();
+                this.colorFrameReader.FrameArrived += colorFrameReader_FrameArrived;
+                this.kinect.Open();//キネクト起動！！
+                if (!kinect.IsOpen)
+                {
+                    width = colorFrameDescription.Width;
+                    height = colorFrameDescription.Height;
+                    bytesPerPixel = colorFrameDescription.BytesPerPixel;
+                }
+                else
+                {
+                    this.errorLog.Visibility = Visibility.Visible;
+                    this.errorLog.Content = "キネクトが見つからないよ！残念！";
+                    throw new Exception("キネクトが見つかりませんでした！！！");
+                }
+                ///bodyを格納するための配列作成
+                bodies = new Body[kinect.BodyFrameSource.BodyCount];
+
+                ///ボディリーダーを開く
+                bodyFrameReader = kinect.BodyFrameSource.OpenReader();
+                bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+
+        }
+
+        void bodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void colorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            ColorFrame colorFrame = e.FrameReference.AcquireFrame();
+
+            if (colorFrame == null)
+                return;
+            /*以下はカラーデータの取得等の処理が入る・・・らしい*/
+            byte[] colors = new byte[width * height * bytesPerPixel];
+            colorFrame.CopyConvertedFrameDataToArray(colors, this.colorImageFormat);
+            /*ここの処理が複雑なので解説、(画像の横幅指定,画像の縦幅指定,dpi...?,dpi?,フォーマット,(たしか)エフェクト,画素データのバッファ量指定,ストライド(横一列)のbyte数指定*/
+            BitmapSource bitmapSource = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, colors, width * (int)bytesPerPixel);
+            
+            /*ここまで*/
+            colorFrame.Dispose();//データの破棄、メモリの解放
+        }
+    }
+}
